8ac24fb8bfcb3663f901f849be41d6c7e2533151
pkolt <pkoltyshev@gmail.com>
2020-09-09 23:02:19 +0500

    Add react router

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignIn = () => {
+    return (
+        <div className="container">
+            <h1>SignIn</h1>
+        </div>
+    );
+}
+
+export default SignIn;
